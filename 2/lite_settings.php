f37764ed5dbefa9fa95289ab3dad215cc3cc95da
mustafa0000 <mustafa.hosny@gmail.com>
2017-02-23 10:09:24 +0300

    kau

--- 2/lite_settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$email_to = "mustafa.hosny@gmail.com"; // your email address
-$email_subject = "Contact Form Message"; // email subject line
-$thankyou = "thankyou.htm"; // thank you page
-
-?>
