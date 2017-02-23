c0474c2fabdd7e106b0318aeaf8725d28fdbf508
mustafa0000 <mustafa.hosny@gmail.com>
2017-02-23 10:01:01 +0300

    sasr

---
+++ 2/fourm/lite_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "mustafa.hosny@gmail.com"; // your email address
+$email_subject = "Contact Form Message"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+?>
